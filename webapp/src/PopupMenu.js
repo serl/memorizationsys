c1528394eff9f90456655b9f648ca04a5bd85a92
Sergio Livi <me@serl.it>
2019-06-20 12:38:56 +0200

    ask for confirmation for delete/reset actions

--- webapp/src/PopupMenu.js
+++ webapp/src/PopupMenu.js
@@ -1,12 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import IconButton from '@material-ui/core/IconButton'
 import Menu from '@material-ui/core/Menu'
 import MenuItem from '@material-ui/core/MenuItem'
 import MoreVertIcon from '@material-ui/icons/MoreVert'
+import ConfirmDialog from './ConfirmDialog'
+
+function useSelectedOption() {
+  const [selOption, setSelOption] = useState()
+  const [showOption, setShowOption] = useState(false)
+  const setOption = opt => {
+    if (opt) {
+      setSelOption(opt)
+      setShowOption(true)
+    } else {
+      setShowOption(false)
+    }
+  }
+  return [selOption, showOption, setOption] // setOption(null) will unset showOption, leaving selOption alone
+}
 
 function PopupMenu({ options = [], ...props }) {
-  const [anchorEl, setAnchorEl] = React.useState(null)
+  const [anchorEl, setAnchorEl] = useState()
   const open = Boolean(anchorEl)
+  const [selOption, showOption, setSelOption] = useSelectedOption()
 
   function handleClick(event) {
     setAnchorEl(event.currentTarget)
@@ -36,11 +52,29 @@ function PopupMenu({ options = [], ...props }) {
         {...props}
       >
         {options.map((option, i) => (
-          <MenuItem key={i} onClick={() => { handleClose(); option.action && option.action() }}>
+          <MenuItem
+            key={i}
+            onClick={() => {
+              handleClose()
+              setSelOption(option)
+            }}
+          >
             {option.name}
           </MenuItem>
         ))}
       </Menu>
+      {selOption &&
+        <ConfirmDialog
+          open={showOption}
+          onClose={() => {
+            setSelOption(null)
+          }}
+          handleAction={() => {
+            setSelOption(null)
+            selOption.action && selOption.action()
+          }}
+        />
+      }
     </>
   )
 }
