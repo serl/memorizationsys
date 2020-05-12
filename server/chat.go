5d8bb52724ac5a774464799da33310cc2b04d21a
Sergio Livi <me@serl.it>
2020-05-12 16:35:41 +0200

    add web URL to bot commands

--- server/chat.go
+++ server/chat.go
@@ -38,8 +38,8 @@ func HandleMessage(msg *tgbotapi.Message) {
 			return u.SetAndShowState(c, UserSetup, nil)
 		} else if strings.HasPrefix(msg.Text, "/settings") {
 			return u.SetAndShowState(c, Settings, nil)
-		} else if strings.HasPrefix(msg.Text, "/token") {
-			return u.SetAndShowState(c, AskForToken, nil)
+		} else if strings.HasPrefix(msg.Text, "/web") {
+			return u.SetAndShowState(c, AskForWebApp, nil)
 		}
 
 		switch u.State {
@@ -426,8 +426,6 @@ func HandleMessage(msg *tgbotapi.Message) {
 				reply("I don't understand what you mean, please try again.", nil)
 			}
 			return u.SetAndShowState(c, Settings, nil)
-		case AskForToken:
-			return u.SetAndShowState(c, DeckList, nil)
 		default:
 			reply("You're in a weird state", noKeyboard)
 			return nil
